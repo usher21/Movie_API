9518297f53ae495d63a1f1476786fa6c5e70fc90
Mar Ndiaye <mar.ndiaye221@gmail.com>
2023-03-17 16:01:49 +0000

    Ajout de la fonctionnalité de scroll infini

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const input = document.querySelector('input[type="text"]')
 const emptyResult = document.querySelector('.empty-result')
 const moviesContainer = document.querySelector('.movies-container')
-const page = Math.floor(Math.random() * 235)
+let page = 98
 const searchAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=${page}`
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page='
 
 let data = []
+let movieArray = []
+let searchMovies = []
 
-showResults(API_URL)
+showResults(API_URL + page)
+
+const observer = new IntersectionObserver((entries) => {
+    for (const entry of entries)
+        if (entry.isIntersecting)
+            showResults(API_URL + (++page))
+})
 
 input.addEventListener('input', () => {
     moviesContainer.innerHTML = ''
+    
+    observer.unobserve(document.querySelector('.observe'))
+
     let search = input.value
     if (search == '') {
-        showResults(API_URL)
+        page = 77
+        movieArray = []
+        showResults(API_URL + page)
+        observer.observe(document.querySelector('.observe'))
         return
     }
 
+    searchMovies = []
+    
     fetchMovies(searchAPI + search)
         .then(movie => {
-            return showData(movie.results)
+            return showSearchMovies(movie.results)
         }).then(() => {
             if (!hasChild()) {
                 emptyResult.style.display = 'block'
@@ -46,9 +62,8 @@ async function fetchMovies(url) {
 function showResults(url) {
     const movies = fetchMovies(url)
     movies
-        .then(movie => {
-            showData(movie.results)
-        })
+        .then(movie => showData(movie.results))
+        .then(() => observer.observe(document.querySelector('.observe')))
         .catch(error => console.log(error.message))
 }
 
@@ -59,27 +74,110 @@ function hasChild() {
 function showData(movies) {
     for (const movie of movies) {
         const imgUrl = 'https://image.tmdb.org/t/p/w220_and_h330_face/'
-        if (movie.poster_path != null) {
-            const oneMovie = createMovieComponent(movie.original_title, movie.vote_average, movie.overview, imgUrl + movie.poster_path)
-            moviesContainer.append(oneMovie)
-        } else {
-            const oneMovie = createMovieComponent(movie.original_title, movie.vote_average, movie.overview, 'notfound.jpg')
-            moviesContainer.append(oneMovie)
+
+        const oneMovie = createMovieComponent('', '', '', '')
+        moviesContainer.append(oneMovie)
+        
+        const movieInfos = {}
+        movieInfos.original_title = movie.original_title
+        movieInfos.vote_average  = movie.vote_average
+        movieInfos.overview = movie.overview
+
+        if (movie.poster_path != null)
+            movieInfos.img = imgUrl + movie.poster_path
+        else
+            movieInfos.img = 'notfound.jpg'
+        movieArray.push(movieInfos)
+    }
+    setTimeout(() => {
+        removeAnimation()
+        let i = 0
+        for (const movie of movieArray) {
+            fillMovieInfos(movie.original_title , movie.vote_average, movie.overview, movie.img, i++)
         }
+    }, 2000);
+}
+
+/*-----------------------------------------------------------------------------------------------------------------*/
+
+function showSearchMovies(movies) {
+    for (const movie of movies) {
+        
+        const imgUrl = 'https://image.tmdb.org/t/p/w220_and_h330_face/'
+
+        const oneMovie = createMovieComponent('', '', '', '')
+        moviesContainer.append(oneMovie)
+        
+        const movieInfos = {}
+        movieInfos.original_title = movie.original_title
+        movieInfos.vote_average  = movie.vote_average
+        movieInfos.overview = movie.overview
+
+        if (movie.poster_path != null)
+            movieInfos.img = imgUrl + movie.poster_path
+        else
+            movieInfos.img = 'notfound.jpg'
+        searchMovies.push(movieInfos)
+    }
+    setTimeout(() => {
+        removeAnimation()
+        let i = 0
+        for (const movie of searchMovies) {
+            fillMovieInfos(movie.original_title , movie.vote_average, movie.overview, movie.img, i++)
+        }
+    }, 2000);
+}
+
+/*-----------------------------------------------------------------------------------------------------------------*/
+
+function removeAnimation() {
+    const childs = moviesContainer.children
+    for (const child of childs) {
+        child.classList.remove('loading')
+        const firstChild = child.firstElementChild
+        const lastChild = child.lastElementChild
+        firstChild.classList.remove('loading')
+        lastChild.classList.remove('loading')
     }
 }
 
+/*-----------------------------------------------------------------------------------------------------------------*/
+
+function fillMovieInfos(title, rating, contentOverview, poster_path, index) {
+    const movies = moviesContainer.children
+    const movie = movies[index]
+    if (movie) {
+        movie.style.backgroundImage = `url('${poster_path}')`
+        const titleContainer = movie.firstElementChild
+        const titleText = titleContainer.firstElementChild
+        titleText.textContent = title
+        titleText.style.backgroundColor = 'rgb(85, 15, 151)'
+        const rate = titleContainer.lastElementChild
+        rate.textContent = rating
+        rate.style.backgroundColor = 'rgb(50, 2, 95)'
+
+        const overviewContainer = movie.lastElementChild
+        overviewContainer.style.backgroundColor = '#FFF'
+        const overviewTitle = overviewContainer.firstElementChild
+        overviewTitle.textContent = 'Overview'
+        const overviewContent = overviewContainer.lastChild
+        overviewContent.textContent = contentOverview
+    }
+}
+
+/*-----------------------------------------------------------------------------------------------------------------*/
+
 function createMovieComponent(title, rating, contentOverview, poster_path) {
-    const movieContainer = createElement('div', { class: 'movie' })
+    const movieContainer = createElement('div', { class: 'movie loading' })
     movieContainer.style.backgroundImage = `url('${poster_path}')`
 
-    const titleMovie = createElement('div', { class: 'title-movie'})
+    const titleMovie = createElement('div', { class: 'title-movie loading'})
     const titleContent = createElement('h3', { class: 'title'}, title)
     const rate = createElement('span', { class: 'rate'}, rating)
     titleMovie.append(titleContent)
     titleMovie.append(rate)
 
-    const overviewContainer = createElement('div', { class: 'overview-container'})
+    const overviewContainer = createElement('div', { class: 'overview-container loading'})
     const overviewTitle = createElement('h3', { class: 'overview-title'}, 'Overview')
     const overviewContent = createElement('p', { class: 'overview-content'}, contentOverview)
     overviewContainer.append(overviewTitle)
@@ -91,6 +189,8 @@ function createMovieComponent(title, rating, contentOverview, poster_path) {
     return movieContainer
 }
 
+/*-----------------------------------------------------------------------------------------------------------------*/
+
 function createElement(tagName, attributs = {}, content = '') {
     const element = document.createElement(tagName)
     for (const attribut in attributs) {
@@ -100,3 +200,4 @@ function createElement(tagName, attributs = {}, content = '') {
     return element
 }
 
+/*-----------------------------------------------------------------------------------------------------------------*/
